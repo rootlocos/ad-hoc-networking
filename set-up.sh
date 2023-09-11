2fd2e6a9bb08270cb8e494d9d198883ce32ed2f3
zivmehadady <91672035+zivmehadady@users.noreply.github.com>
2023-09-11 17:44:31 +0300

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -5,6 +5,7 @@ sudo apt-get install python3-pip
 pip3 --version
 python3 "-m" "pip" "install" "-U" "poetry==1.1.4" --only-binary cryptography
 pip install paramiko
+pip install difflib
 
 sudo apt-get install git-all
 git version
